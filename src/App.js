186c9a38fcd23c79d6593a2d420e4f30f3a07a08
Julia Smikalo <JuliaSmikalo@gmail.com>
2017-11-27 00:43:47 +0200

    Code style

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, {Component} from 'react';
 import BEM from 'helpers/bem';
 import Header from 'cmps/Header/Header';
 import Content from 'cmps/Content/Content';
-import './App.scss';
 import Footer from 'cmps/Footer/Footer';
 import Login from 'cmps/Login/Login';
 import Register from 'cmps/Register/Register';
 import QuestionPage from 'cmps/QuestionPage/QuestionPage';
+import QuestionsList from 'cmps/QuestionsList/QuestionsList';
 import HomePage from 'cmps/HomePage/HomePage';
 import {BrowserRouter, Route} from 'react-router-dom';
+import './App.scss';
 
 
 const bem = new BEM('app');
@@ -19,12 +20,15 @@ export default class App extends Component {
       <BrowserRouter>
         <div className={bem}>
           <Header/>
+
           <Content>
             <Route path='/' component={HomePage} exact/>
+            <Route path='/questions' component={QuestionsList}/>
             <Route path='/question/:id' component={QuestionPage}/>
             <Route path='/login' component={Login}/>
             <Route path='/signup' component={Register}/>
           </Content>
+
           <Footer/>
         </div>
       </BrowserRouter>
