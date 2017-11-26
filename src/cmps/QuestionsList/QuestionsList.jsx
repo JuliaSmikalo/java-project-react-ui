7907d63a0488627a010b4afb5158e4c6634d5aea
Julia Smikalo <JuliaSmikalo@gmail.com>
2017-11-27 00:47:09 +0200

    Move short question details to seperated component

--- src/cmps/QuestionsList/QuestionsList.jsx
+++ src/cmps/QuestionsList/QuestionsList.jsx
@@ -1,10 +1,8 @@
 import React, {Component} from 'react';
 import BEM from '../../helpers/bem';
 import './QuestionsList.scss';
-import {Link} from 'react-router-dom';
-import {Label} from 'react-bootstrap';
-import links from '../../configs/links';
 import PageHeader from '../PageHeader/PageHeader';
+import ShortQuestion from '../ShortQuestion/ShortQuestion';
 
 
 const bem = new BEM('questions-list');
@@ -13,50 +11,16 @@ export default class QuestionsList extends Component {
   render() {
     return (
       <div className={bem}>
-        <PageHeader>Last questions</PageHeader>
-        {this.renderQuestion()}
-        {this.renderQuestion()}
-        {this.renderQuestion()}
+        <PageHeader>All Questions</PageHeader>
+        <ShortQuestion/>
+        <ShortQuestion/>
+        <ShortQuestion/>
+        <ShortQuestion/>
+        <ShortQuestion/>
+        <ShortQuestion/>
+        <ShortQuestion/>
+        <ShortQuestion/>
       </div>
     );
-  }
-
-  renderQuestion() {
-    return (
-      <article className={bem.elem('question')}>
-        <div className={bem.elem('ratings')}>
-          {this.renderRating('rating', -1)}
-          {this.renderRating('answers', 23)}
-          {this.renderRating('views', 10)}
-        </div>
-        <div className={bem.elem('details')}>
-          <header>
-            <Link to={links.question(1)} className={bem.elem('header-link')}>
-              iOS 6 iAd orientation issues - iAds rotate my portrait only app to landscape and leaves it that way
-            </Link>
-          </header>
-          <div className={bem.elem('text')}>
-              Using this code to allow the media players to rotate in landscape (which is not supported by the app) while they are in
-              fullscreen working fine in ios 10 but since ios 11 the screen will not rotate back after leaving fullscreen, thus not resizing
-              the ui (the app will after rotation occupate only half of the screen). It seems there were some modification on avkit but i
-              cannot find any ressources on this, thoughts?
-          </div>
-          <div className={bem.elem('tags')}>
-            <Label bsStyle="info">Info</Label>&nbsp;
-            <Label bsStyle="info">Heart</Label>&nbsp;
-            <Label bsStyle="info">Fishing</Label>&nbsp;
-          </div>
-        </div>
-      </article>
-    )
-  }
-
-  renderRating(name, value) {
-    return (
-      <div className={bem.elem('rating')}>
-        <div className={bem.elem('rating-value')}>{value}</div>
-        <div className={bem.elem('rating-name')}>{name}</div>
-      </div>
-    )
   }
 }
